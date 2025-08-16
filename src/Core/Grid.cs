4588a17260af5e2b0310a6911e894436b91382cb
clement lion <clement.lionceau@gmail.com>
2025-08-16 19:00:56 +0200

    finished first version of game of life

--- src/Core/Grid.cs
+++ src/Core/Grid.cs
@@ -1,16 +1,16 @@
 using Microsoft.Extensions.Logging;
-using Raylib_cs;
+using System.Numerics;
 
-public class Grid : UI.IDrawable
+public class Grid
 {
     // Debug variables
     private readonly ILogger _logger = Logger.CreateLogger<Grid>();
 
     // Cell variables
-    public int cellSize { get; private set; }
-    public int columns { get; private set; }
-    public int rows { get; private set; }
-    private bool[,] cells;
+    public int cellSize { get; }
+    public int columns { get; }
+    public int rows { get; }
+    public bool[,] cells { get; private set; }
 
     public Grid(int columns, int rows, int cellSize)
     {
@@ -29,7 +29,7 @@ public class Grid : UI.IDrawable
         if (row >= rows) return false;
         return true;
     }
-    
+
     public bool GetCell(int column, int row)
     {
         bool validInput = CheckColumnRowValidity(column, row);
@@ -49,9 +49,30 @@ public class Grid : UI.IDrawable
         }
         else
         {
+            _logger.LogInformation($@"Tried to update value at column and row {column}, {row} 
+            But grid only has dimensions {columns}, {rows}.");
             throw new IndexOutOfRangeException($@"Tried to update value at column and row {column}, {row} 
             But grid only has dimensions {columns}, {rows}.");
         }
+    }
+
+    public Vector2 ToWorld(int column, int row)
+    {
+        bool validInput = CheckColumnRowValidity(column, row);
+        if (!validInput)
+        {
+            throw new ArgumentException("Invalid input.");
+        }
+        Vector2 answerVector = new();
+        answerVector.X = column * cellSize;
+        answerVector.Y = row * cellSize;
+        return answerVector;
+    }
+    public (int, int) ToGrid(Vector2 position)
+    {
+        int column = (int)(position.X / cellSize);
+        int row = (int)(position.Y / cellSize);
+        return (column, row);
     }
 
     public bool[] MooreNeighborhood(int column, int row)
@@ -74,8 +95,23 @@ public class Grid : UI.IDrawable
         return answerList;
     }
 
-    public void Draw()
+    public int MooreNeighborhoodCount(int column, int row)
     {
+        int count = 0;
+        var answerList = MooreNeighborhood(column, row);
+        for (int i = 0; i < answerList.Length; i++)
+        {
+            if (answerList[i])
+            {
+                count++;
+            }
+        }
+        return count;
+    }
 
+    public void UpdateGrid(bool[,] nextCells)
+    {
+        _logger.LogInformation("Grid updated.");
+        cells = nextCells;
     }
 }
--- src/Core/Rules.cs
+++ src/Core/Rules.cs
@@ -1,6 +1,9 @@
 using Microsoft.Extensions.Logging;
+using Raylib_cs;
+using System.Numerics;
+using UI;
 
-class GameOfLifeRule
+class GameOfLifeRule : IDrawable
 {
     private readonly ILogger _logger = Logger.CreateLogger<GameOfLifeRule>();
 
@@ -29,6 +32,51 @@ class GameOfLifeRule
 
     public void Update()
     {
-        
+        _logger.LogInformation("Updating game of life.");
+        int interCount;
+        bool interGridCellValue;
+        bool[,] nextCells = (bool[,])gameOfLifeGrid.cells.Clone();
+        for (int interRows = 0; interRows < gameOfLifeGrid.rows; interRows++)
+        {
+            for (int interColumns = 0; interColumns < gameOfLifeGrid.columns; interColumns++)
+            {
+                interCount = gameOfLifeGrid.MooreNeighborhoodCount(interRows, interColumns);
+                interGridCellValue = gameOfLifeGrid.GetCell(interRows, interColumns);
+                if (interGridCellValue)
+                {
+                    if (interCount < 2 || interCount > 3)
+                    {
+                        nextCells[interRows, interColumns] = false;
+                    }
+                }
+                else
+                {
+                    if (interCount == 3)
+                    {
+                        nextCells[interRows, interColumns] = true;
+                    }
+                }
+            }
+        }
+        gameOfLifeGrid.UpdateGrid(nextCells);
+        _logger.LogInformation("Game of life updated.");
+    }
+
+    public void Draw()
+    {
+        bool interCell;
+        Vector2 cellPosition;
+        for (int interRows = 0; interRows < gameOfLifeGrid.rows; interRows++)
+        {
+            for (int interColumns = 0; interColumns < gameOfLifeGrid.columns; interColumns++)
+            {
+                interCell = gameOfLifeGrid.GetCell(interRows, interColumns);
+                if (interCell)
+                {
+                    cellPosition = gameOfLifeGrid.ToWorld(interRows, interColumns);
+                    Raylib.DrawRectangle((int)cellPosition.X, (int)cellPosition.Y, gameOfLifeGrid.cellSize, gameOfLifeGrid.cellSize, Color.White);
+                }
+            }
+        }
     }
 }
