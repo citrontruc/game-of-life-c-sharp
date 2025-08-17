01cb8d40fdeb5f9c501dcbb8f8d06881611976f7
clement lion <clement.lionceau@gmail.com>
2025-08-17 12:33:32 +0200

    finished userInput

--- src/UI/UserInput.cs
+++ src/UI/UserInput.cs
@@ -4,9 +4,11 @@ using System.Numerics;
 
 public class UserInput
 {
-    public Vector2 MousePosition { get; set; }
-    public bool LeftClick { get; set; }
-    public bool RightClick { get; set; }
-    public bool Select { get; set; }
-    public bool Pause { get; set; }
+    public Vector2 MousePosition;
+    public bool LeftClick;
+    public bool RightClick;
+    public bool Input;
+    public bool MoveX;
+    public bool MoveY;
+    public bool Pause;
 }
